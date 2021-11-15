b3d0c2d30cd9b6cc77d6cc2c54c6fc099d5b2045
YuriDyachenko <yuri.dyachenko@outlook.com>
2021-11-15 10:01:03 +0300

    hw-09: delete navigation module

--- app/src/main/java/yuri/dyachenko/githubclient/di/AppComponent.kt
+++ app/src/main/java/yuri/dyachenko/githubclient/di/AppComponent.kt
@@ -12,7 +12,6 @@ import javax.inject.Singleton
     modules = [
         ContextModule::class,
         CiceroneModule::class,
-        NavigationModule::class,
         NetworkStatusModule::class,
         RoomModule::class,
         RetrofitModule::class,
